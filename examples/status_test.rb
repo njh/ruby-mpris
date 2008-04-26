7d2cc9c0990d4ca1feb06365cbbf372caea1cefe
Nicholas J Humfrey <njh@aelius.com>
2008-04-26 20:25:24 +0000

    Script to test that status checks are working.

---
+++ examples/status_test.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby
+#
+# Script to test the repeat/loop/random getters and setters.
+#
+# Author::    Nicholas J Humfrey  (mailto:njh@aelius.com)
+# Copyright:: Copyright (c) 2008 Nicholas J Humfrey
+# License::   Distributes under the same terms as Ruby
+#
+
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'mpris'
+
+
+mpris = Mpris.new
+
+
+puts "Enabling looping."
+mpris.tracklist.loop = true
+puts " Looping status: #{mpris.tracklist.loop}"
+
+puts "Disabling looping."
+mpris.tracklist.loop = false
+puts " Looping status: #{mpris.tracklist.loop}"
+
+puts "Enabling random."
+mpris.tracklist.random = true
+puts " Random status: #{mpris.tracklist.random}"
+
+puts "Disabling random."
+mpris.tracklist.random = false
+puts " Random status: #{mpris.tracklist.random}"
+
+puts "Enabling repeating."
+mpris.player.repeat = true
+puts " Looping status: #{mpris.player.repeat}"
+
+puts "Disabling repeating."
+mpris.player.repeat = false
+puts " Looping status: #{mpris.player.repeat}"
+
+
+puts "Stopping playback..."
+mpris.player.stop
+puts " Mpris::Player::STOPPED: #{Mpris::Player::STOPPED}"
+puts " Playback status: #{mpris.player.status}"
+
+puts "Starting playback..."
+mpris.player.play
+puts " Mpris::Player::PLAYING: #{Mpris::Player::PLAYING}"
+puts " Playback status: #{mpris.player.status}"
+
+# Current crashes VLC:
+#puts "Pausing playback..."
+#mpris.player.pause
+#puts " Mpris::Player::PAUSED: #{Mpris::Player::PAUSED}"
+#puts " Playback status: #{mpris.player.status}"
