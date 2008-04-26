cedbdb6a83554d143467d34d1ed056e13dee39e1
Nicholas J Humfrey <njh@aelius.com>
2008-04-26 20:20:35 +0000

    Added script which tests for the capabilites of the media player.

---
+++ examples/capabilities_test.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+#
+# Script to display the cababilites of the media player.
+#
+# Author::    Nicholas J Humfrey  (mailto:njh@aelius.com)
+# Copyright:: Copyright (c) 2008 Nicholas J Humfrey
+# License::   Distributes under the same terms as Ruby
+#
+  
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'mpris'
+
+
+mpris = Mpris.new
+puts "Checking capabilites:"
+puts " can_go_next?: #{mpris.player.can_go_next?}"
+puts " can_go_prev?: #{mpris.player.can_go_prev?}"
+puts " can_pause?: #{mpris.player.can_pause?}"
+puts " can_play?: #{mpris.player.can_play?}"
+puts " can_seek?: #{mpris.player.can_seek?}"
+puts " can_provide_metadata?: #{mpris.player.can_provide_metadata?}"
+puts " has_tracklist?: #{mpris.player.has_tracklist?}"
