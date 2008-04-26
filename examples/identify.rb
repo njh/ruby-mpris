d063f315bbd60d876d955cb2f4680552d2ad85df
Nicholas J Humfrey <njh@aelius.com>
2008-04-26 20:19:11 +0000

    Added boilerplate to top of file.

--- examples/identify.rb
+++ examples/identify.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/ruby
+#
+# Script to display name of the media player and supported MPRIS API version.
+#
+# Author::    Nicholas J Humfrey  (mailto:njh@aelius.com)
+# Copyright:: Copyright (c) 2008 Nicholas J Humfrey
+# License::   Distributes under the same terms as Ruby
+#
 
 $:.unshift File.dirname(__FILE__)+'/../lib'
 
