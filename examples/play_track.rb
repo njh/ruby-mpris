af8f47d1880ef1b07ca450245b5fe59d620a72f4
Nicholas J Humfrey <njh@aelius.com>
2008-04-26 20:19:39 +0000

    Added example which just starts plaing a file immediately

---
+++ examples/play_track.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+#
+# Script to immediately start playing a track in the media player.
+#
+# Author::    Nicholas J Humfrey  (mailto:njh@aelius.com)
+# Copyright:: Copyright (c) 2008 Nicholas J Humfrey
+# License::   Distributes under the same terms as Ruby
+#
+
+$:.unshift File.dirname(__FILE__)+'/../lib'
+
+require 'mpris'
+
+raise "Usage: play_track.rb <filename>" unless (ARGV.size == 1)
+
+mpris = Mpris.new
+mpris.tracklist.add_track( ARGV[0], true )
